4c2cb1c96428d80d0bf48123213bede9a9133577
MD RUMEL AHMED <rumelrumel0987654321@gmail.com>
2026-04-07 23:14:56 +0600

    completed stats section

--- src/components/homepage/Banner.jsx
+++ src/components/homepage/Banner.jsx
@@ -3,10 +3,10 @@ import { FaAppStoreIos, FaGooglePlay } from 'react-icons/fa';
 import bannerImg from '../../assets/images/hero.png'
 const Banner = () => {
     return (
-        <div className='w-full md:w-10/12 mx-auto h-[106vh] mt-6'>
-            <div className='text-center space-y-4'>
+      <div className='w-full md:w-10/12 mx-auto min-h-[64vh] flex items-center'>
+            <div className='text-center space-y-4 '>
                 <h2 className='text-3xl md:text-5xl font-bold'> We Build <br /> <span className='text-purple-600'>Productive</span> Apps</h2>
-                <p className='w-8/12 mx-auto'>At HERO.IO, we craft innovative apps designed to make everyday life simpler, smarter, and more exciting.Our goal is to turn your ideas into digital experiences that truly make an impact.</p>
+                <p className='w-full p-2 md:w-8/12 mx-auto'>At HERO.IO, we craft innovative apps designed to make everyday life simpler, smarter, and more exciting.Our goal is to turn your ideas into digital experiences that truly make an impact.</p>
 
               <div className='space-x-4'>
                 <button className='btn btn-outline'> <FaGooglePlay></FaGooglePlay> Google Play</button>
--- src/components/homepage/Stats.jsx
+++ src/components/homepage/Stats.jsx
@@ -2,9 +2,32 @@ import React from 'react';
 
 const Stats = () => {
     return (
-        <div className='h-screen border-2  bg-black'>
-           
+    <div className=' bg-linear-to-r  from-purple-600 to-purple-500 text-center py-20 space-y-8'>
+        <div>
+            <h2 className='text-3xl md:text-5xl font-bold text-white'>Trusted by Millions, Built for You</h2>
         </div>
+       
+              <div className="stats stats-vertical lg:stats-horizontal  w-10/12 mx-auto text-white">
+  <div className="stat ">
+    <div className="stat-title text-white">Downloads</div>
+    <div className="stat-value">31K</div>
+    <div className="stat-desc text-white">Jan 1st - Feb 1st</div>
+  </div>
+
+  <div className="stat">
+    <div className="stat-title text-white">New Users</div>
+    <div className="stat-value">4,200</div>
+    <div className="stat-desc text-white">↗︎ 400 (22%)</div>
+  </div>
+
+  <div className="stat">
+    <div className="stat-title text-white">New Registers</div>
+    <div className="stat-value">1,200</div>
+    <div className="stat-desc text-white">↘︎ 90 (14%)</div>
+  </div>
+</div>
+       
+    </div>
     );
 };
 
